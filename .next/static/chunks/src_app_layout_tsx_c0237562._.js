b24c7230e699b3bfbe4db5d13610d9595871e5b3
luiza152 <luizagoncalves1501sp@gmail.com>
2025-06-23 17:48:24 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750711702768

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__8c5b3ecc._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_components_MobileMenu_tsx_10bda7ce._.js"
+],
+    source: "dynamic"
+});
